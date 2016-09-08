3913e13d09faa932bd89c9ba802c6b81d974d814
DuXianDong <dllo@duxiandong.local>
2016-09-08 20:04:38 +0800

    分包

---
+++ MyApplication/app/src/main/java/com/du/a36kr/model/A.java
@@ -0,0 +1,7 @@
+package com.du.a36kr.model;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class A {
+}
---
+++ MyApplication/app/src/main/java/com/du/a36kr/model/net/n.java
@@ -0,0 +1,7 @@
+package com.du.a36kr.model.net;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class n {
+}
